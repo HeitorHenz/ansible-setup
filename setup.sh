d0fe75c87de2734d992a6a3cf399367d019f6cde
Heitor <heitor1954hg@gmail.com>
2026-02-11 22:20:55 -0300

    .

--- setup.sh
+++ setup.sh
@@ -4,11 +4,9 @@ set -e
 export PATH=$PATH:$HOME/.local/bin
 
 if ! command -v ansible &> /dev/null; then
-  python3 -m ensurepip --upgrade --user
-  export PATH=$PATH:$HOME/.local/bin
+    python3 -m pip install --user ansible
 fi
 
-pip install --user ansible
 ansible-galaxy collection install community.general
 
 ansible-playbook setup.yml --ask-become-pass
