b4633f96156b6d1b5e04cdb1bd32af41c0e7af5f
Heitor <38962918+HeitorHenz@users.noreply.github.com>
2026-02-12 15:20:21 -0300

    Remove Ansible installation from setup script

--- setup.sh
+++ setup.sh
@@ -8,10 +8,6 @@ if !command -v uv &> /dev/null; then
     export PATH=$PATH:$HOME/.local/bin
 fi
 
-if !command -v ansible &> /dev/null; then
-    uv tool install ansible-core --with ansible
-fi
-
 # Install starship -> move to cargo eventually
 curl -sS https://starship.rs/install.sh | sh
 
