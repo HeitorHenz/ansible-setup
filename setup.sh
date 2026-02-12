0321844ac19d8e260fc4990d0b365e7d3edc8cb7
Heitor <heitor1954hg@gmail.com>
2026-02-11 22:03:25 -0300

    Update setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ export PATH=$PATH:$HOME/.local/bin
 if ! command -v ansible &> /dev/null; then
   python3 -m ensurepip --upgrade --user
   pip install --user ansible
+  export PATH=$PATH:$HOME/.local/bin
 fi
 
 ansible-galaxy collection install community.general
