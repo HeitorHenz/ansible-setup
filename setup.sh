fefc673266ce04fb94d17627a895f01aa7b4a18a
Heitor <heitor1954hg@gmail.com>
2026-02-24 08:58:40 -0300

    progress

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Install Ansible
-sudo dnf install ansible ansible-collection-community-general -y
+sudo dnf install -y ansible ansible-collection-community-general
 
 # Clone dotfiles
-git clone https://github.com/YOUR_USERNAME/dotfiles.git ~/.dotfiles
+git clone https://github.com/HeitorHenz/.dotfiles ~/.dotfiles
 
 # run Ansible
 curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
