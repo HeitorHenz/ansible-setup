a39f8c328c914b9e2de7bada3ff9bb9ede53e404
Heitor <heitor1954hg@gmail.com>
2026-02-23 21:55:48 -0300

    Switch to stow

--- setup.sh
+++ setup.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 set -e
 
-# Install uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH=$PATH:$HOME/.local/bin
-
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> "$HOME/.bashrc"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
-
 # Install Ansible
-uv tool install ansible-core --with ansible
+sudo dnf install ansible ansible-collection-community-general
 
-# Run ansible
-ansible-galaxy collection install community.general
+# Clone dotfiles
+git clone https://github.com/YOUR_USERNAME/dotfiles.git ~/.dotfiles
 
+# run Ansible
 curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
-
 ansible-playbook setup.yml --ask-become-pass
-
-systemctl reboot
