1b211358d84e749dfaea5881825643fae80df043
Heitor <38962918+HeitorHenz@users.noreply.github.com>
2026-04-06 16:08:02 -0300

    Remove dotfiles cloning from setup.sh
    Removed dotfiles cloning step from setup script.

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ set -e
 # Update package database and install Ansible, Git, and curl
 sudo pacman -Sy --noconfirm ansible git curl
 
-# Clone dotfiles
-git clone https://github.com/HeitorHenz/.dotfiles ~/.dotfiles
-
 # Run Ansible
 curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
 ansible-playbook setup.yml --ask-become-pass
