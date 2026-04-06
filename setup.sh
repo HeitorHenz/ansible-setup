58e801209650fbd25f3e1da8b92e09455cfed96b
Heitor <heitor1954hg@gmail.com>
2026-04-06 19:46:35 -0300

    update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-# Update package database and install Ansible, Git, and curl
 sudo pacman -Sy --noconfirm ansible git curl
 
-# Run Ansible
 curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
 ansible-playbook setup.yml --ask-become-pass
