ef89230ee5008150ca8b43274eededc3c4da873f
Heitor <38962918+HeitorHenz@users.noreply.github.com>
2026-04-06 15:46:48 -0300

    Modify setup script for package installation
    Updated package installation commands for Ansible, Git, and curl.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# Install Ansible
-sudo dnf install -y ansible ansible-collection-community-general
+# Update package database and install Ansible, Git, and curl
+sudo pacman -Sy --noconfirm ansible git curl
 
 # Clone dotfiles
 git clone https://github.com/HeitorHenz/.dotfiles ~/.dotfiles
 
-# run Ansible
+# Run Ansible
 curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
 ansible-playbook setup.yml --ask-become-pass
