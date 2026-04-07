4fbc26920a704040062d1ab8ca3681a2c640ea2e
h <heitor1954hg@gmail.com>
2026-04-07 17:01:32 -0300

    fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,10 @@
 if status is-interactive
     starship init fish | source
+
     alias hx="helix"
     alias ls="eza --icons --git --group-directories-first"
     alias cat="bat"
-
+    alias zellij="zellij --layout compact"
 end
 
 # uv
