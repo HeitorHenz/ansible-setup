50365504b41c74b6cc4a71dcc37fe5a107046be6
h <heitor1954hg@gmail.com>
2026-04-07 19:06:40 -0300

    .

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-read -rsp "Enter sudo password: " BECOME_PASS
-echo
-
-echo "$BECOME_PASS" | sudo -S pacman -Sy --noconfirm ansible git curl
+sudo pacman -Sy --noconfirm ansible git curl
 
 curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
-ansible-playbook setup.yml -e "ansible_become_password=$BECOME_PASS"
+ansible-playbook setup.yml --ask-become-pass
