2f8f5f9651c8660a1cf28c0594d22fd63cbd0ec4
Heitor <38962918+HeitorHenz@users.noreply.github.com>
2026-04-09 12:39:32 -0300

    Update setup.sh to include base-devel and adjust commands
    Added 'base-devel' package to installation commands and removed 'sudo' for yay-bin installation.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-sudo pacman -Sy --noconfirm ansible git curl
+sudo pacman -Sy --noconfirm ansible git curl base-devel
 
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin && cd /tmp/yay-bin && makepkg -si && rm -rf /tmp/yay-bin
+git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin && cd /tmp/yay-bin && makepkg -si && rm -rf /tmp/yay-bin
 curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
 
 ansible-playbook setup.yml --ask-become-pass
