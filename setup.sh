140ba2346dd8cf2415cb1ad6e1497b35059a3281
Heitor <38962918+HeitorHenz@users.noreply.github.com>
2026-04-09 20:27:11 -0300

    Remove community.general collection installation
    Removed installation of the community.general Ansible collection.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ set -e
 sudo pacman -Syu --noconfirm ansible git curl base-devel
 
 ansible-galaxy collection install kewlfft.aur
-ansible-galaxy collection install community.general
 
 bash -c "$(curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml)"
 
