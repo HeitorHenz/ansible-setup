05aac085ec70c69eabfff4e76232f4a63c5aa7d2
h <heitor1954hg@gmail.com>
2026-04-09 13:40:11 -0300

    progress

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ sudo pacman -Sy --noconfirm ansible git curl base-devel
 rm -rf /tmp/yay-bin
 git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin && cd /tmp/yay-bin && makepkg -si --noconfirm && rm -rf /tmp/yay-bin
 
-curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.sh)"
 
 ansible-playbook setup.yml --ask-become-pass
