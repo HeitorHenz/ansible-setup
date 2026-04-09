12b92ec9234d9fd88eafc1f79b25d99c1dd2606f
Heitor <38962918+HeitorHenz@users.noreply.github.com>
2026-04-09 12:54:48 -0300

    Add --noconfirm to makepkg command in setup.sh
    Updated makepkg command to use --noconfirm option.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -e
 sudo pacman -Sy --noconfirm ansible git curl base-devel
 
 rm -rf /tmp/yay-bin
-git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin && cd /tmp/yay-bin && makepkg -si && rm -rf /tmp/yay-bin
+git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin && cd /tmp/yay-bin && makepkg -si --noconfirm && rm -rf /tmp/yay-bin
 
 curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml
 
