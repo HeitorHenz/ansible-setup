cd7c349b95a5202f432e8a78656ff29b34b1ce8b
Heitor <38962918+HeitorHenz@users.noreply.github.com>
2026-08-27 22:38:54 -0300

    Update setup.sh to comment out installation commands
    Comment out the installation of the kewlfft.aur collection and add alternative installation commands.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ set -e
 
 sudo pacman -Syu --noconfirm ansible git curl base-devel
 
-ansible-galaxy collection install kewlfft.aur
+# curl -fsSL https://install.danklinux.com | sh
+
+# curl -LsSf https://astral.sh/uv/install.sh | sh
 
 bash -c "$(curl -sSL -o setup.yml https://raw.githubusercontent.com/heitorhenz/ansible-setup/main/setup.yml)"
 
